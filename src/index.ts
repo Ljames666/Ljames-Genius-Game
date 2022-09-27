479b478dcf936395c5dc5696b4de5c47b9ead716
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-09-27 12:57:50 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -189,7 +189,7 @@ bottomRight.addEventListener('click', (event) => {
 function check() {
   if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) good = false;
 
-  if (playerOrder.length == 3 && good) {
+  if (playerOrder.length == 20 && good) {
     winGame();
   }
 
